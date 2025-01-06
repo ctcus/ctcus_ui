435fda435063a5e1251872afb921306d892c21f9
Suman Saini <sumansaini@Sumans-MacBook-Pro.local>
2025-01-05 20:24:07 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { EventsComponent } from './components/events/events.component';
+import { MembersComponent } from './components/members/members.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
